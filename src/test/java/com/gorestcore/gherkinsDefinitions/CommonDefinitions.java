8be951ac725e5a718f76089e40518d08fa15035d
Nikita Svidrickis <nikita.svidrickis@testdevlab.com>
2022-01-17 14:04:45 +0200

    17.01

--- src/test/java/com/gorestcore/gherkinsDefinitions/CommonDefinitions.java
+++ src/test/java/com/gorestcore/gherkinsDefinitions/CommonDefinitions.java
@@ -1,24 +1,14 @@
 package com.gorestcore.gherkinsDefinitions;
 
 import com.gorestcore.serenitySteps.CommonSteps;
-import cucumber.api.DataTable;
-import cucumber.api.PendingException;
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 import java.io.IOException;
-import java.util.List;
 
 public class CommonDefinitions {
-    @When("^the user is authorised$")
-    public void theUserIsAuthorised() throws IOException {
-        CommonSteps.setAuthToken();
-    }
 
-    @Then("^the user gets status code \"([^\"]*)\"$")
-    public void assertStatusCode(int statusCode) {
-        CommonSteps.assertStatusCode(statusCode);
+    @When("^the user is authorised$")
+    public void authorise() throws IOException {
+        CommonSteps.setAuthToken();
     }
 }
